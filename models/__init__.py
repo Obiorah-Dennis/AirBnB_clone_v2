46b579e809a285071ffaf553b2fee56215cbd2a7
bmn44100 <njerumurimi@gmail.com>
2023-05-11 08:11:56 -0700

    BaseModel class

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" initializes BaseModel """
+
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+storage = FileStorage()
+storage.reload()
