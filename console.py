eb7ec694e6b68a7a30aca19391e7ab46693800ee
Obiorah-Dennis <obiorahdennis@yahoo.com>
2023-05-11 11:39:17 -0700

    console

---
+++ console.py
@@ -0,0 +1,23 @@
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print()  # Print a newline before exiting
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty line"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
