a09cf679141e5c4bff74a097fc5d9164ee5049eb
Obiorah-Dennis <obiorahdennis@yahoo.com>
2023-09-17 09:15:59 -0700

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
+"""This is the user class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
+from sqlalchemy.orm import relationship, backref
 
-""" module for class user that inherits from BaseModel """
 
-from models.base_model import BaseModel
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
 
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
 
-class User(BaseModel):
-    """User class"""
+    places = relationship("Place",
+                          backref="user",
+                          cascade="all, delete, delete-orphan")
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    reviews = relationship("Review",
+                           backref="user",
+                           cascade="all, delete, delete-orphan")
