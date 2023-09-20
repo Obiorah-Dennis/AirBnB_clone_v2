18dbde1406c08385d6f4dd4685fdd18e7a7ff395
Obiorah-Dennis <obiorahdennis@yahoo.com>
2023-09-20 01:13:18 -0700

    DBStorage - Amenity... and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,17 @@
-"""module for class Amenity that inherits from BaseModel"""
+#!/usr/bin/python3
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
-from models.base_model import BaseModel
 
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
 
-class Amenity(BaseModel):
-    """ Amenity class """
-    name = ""
