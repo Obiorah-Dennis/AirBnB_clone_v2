96a751e37d1f8cc9bcaa89e91b644781854de668
Obiorah-Dennis <obiorahdennis@yahoo.com>
2023-10-05 12:20:45 -0700

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,88 +1,24 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
-# sets up my web servers for the deployment of web_static
-
-echo -e "\e[1;32m START\e[0m"
-
-#--Updating the packages
-sudo apt-get -y update
-sudo apt-get -y install nginx
-echo -e "\e[1;32m Packages updated\e[0m"
-echo
-
-#--configure firewall
-sudo ufw allow 'Nginx HTTP'
-echo -e "\e[1;32m Allow incomming NGINX HTTP connections\e[0m"
-echo
-
-#--created the dir
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo -e "\e[1;32m directories created"
-echo
-
-#--adds test string
-echo "<h1>Welcome to www.uniqueel.tech</h1>" > /data/web_static/releases/test/index.html
-echo -e "\e[1;32m Test string added\e[0m"
-echo
-
-#--prevent overwrite
-if [ -d "/data/web_static/current" ];
-then
-    echo "path /data/web_static/current exists"
-    sudo rm -rf /data/web_static/current;
-fi;
-echo -e "\e[1;32m prevent overwrite\e[0m"
-echo
-
-#--create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data
-
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
-echo -e "\e[1;32m Symbolic link created\e[0m"
-echo
-
-#--restart NGINX
-sudo service nginx restart
-echo -e "\e[1;32m restart NGINX\e[0m"
-=======
-# Sets up a web server for deployment of web_static.
-
-apt-get update
-apt-get install -y nginx
-
+# script to configure in order to serve the static pages
+apt-get -y update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+# instructions
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Hello Word" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Fake content
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+# symbolic link to folder erase
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-	alias /data/web_static/current;
-	index index.html index.htm;
-    }
-
-    location /redirect_me {
-	return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
+# change owner and group to ubuntu
+chown -R ubuntu:ubuntu /data/
+# change content of available-default
+sed -i '43i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t autoindex on;\n\t}\n' /etc/nginx/sites-available/default
+# getting changes
 service nginx restart
->>>>>>> 69074df209c2b79906b0eb68be4fd17b107c808a
+exit 0
